bed304a3b33d552deceebef8af6e9736bf4f0ac1
Sebastian R33D <sebastianreed7@gmail.com>
2024-10-22 22:27:04 +0200

    buttons in the main activity with compose

--- app/src/main/java/com/example/greenspot/MainActivity.kt
+++ app/src/main/java/com/example/greenspot/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp  // Import správné jednotky dp
 import com.example.greenspot.ui.theme.GreenSpotTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GreenSpotTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainMenu(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +33,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainMenu(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Button(
+            onClick = { /* TODO: Navigate to Screen 1 */ },
+            modifier = Modifier.padding(16.dp)  // Použití dp pro padding
+        ) {
+            Text(text = "Stránka 1")
+        }
+        Button(
+            onClick = { /* TODO: Navigate to Screen 2 */ },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Stránka 2")
+        }
+        Button(
+            onClick = { /* TODO: Navigate to Screen 3 */ },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Stránka 3")
+        }
+        Button(
+            onClick = { /* TODO: Navigate to Screen 4 */ },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Stránka 4")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainMenuPreview() {
     GreenSpotTheme {
-        Greeting("Android")
+        MainMenu()
     }
 }
